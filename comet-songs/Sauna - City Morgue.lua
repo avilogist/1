2a131d59c0367a338ae2316a699e447675bbcf70
avilogist <rbx.neo@gmail.com>
2025-12-25 15:02:40 +0000

    Create Sauna - City Morgue.lua

---
+++ comet-songs/Sauna - City Morgue.lua
@@ -0,0 +1,17 @@
+-- Thx to https://github.com/XVCHub for the method and songs >.<
+
+if not isfile("Sauna - City Morgue.mp3") then
+    writefile("Sauna - City Morgue.mp3", game:HttpGet("https://github.com/avilogist/1/blob/main/comet-songs/Sauna%20-%20City%20Morgue.mp3"))
+end
+
+local phonk2 = Instance.new("Sound", game:GetService("SoundService"))
+phonk2.SoundId = getcustomasset("blue.mp3")
+phonk2.Volume = getgenv().volume or 1
+phonk2.Looped = getgenv().loop == true
+
+phonk2:Play()
+if not phonk2.Looped then
+    phonk2.Ended:Connect(function()
+        phonk2:Destroy()
+    end)
+end
