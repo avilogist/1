fddeaf88a7612b459728422e7ca200e66580d752
avilogist <rbx.neo@gmail.com>
2025-12-25 15:04:09 +0000

    Create Sliding Hot - Skrapp 2197.lua for audio playback
    Add script to play 'Sliding Hot - Skrapp 2197' song

---
+++ comet-songs/Sliding Hot - Skrapp 2197.lua
@@ -0,0 +1,17 @@
+-- Thx to https://github.com/XVCHub for the method and songs >.<
+
+if not isfile("Sliding Hot - Skrapp 2197.mp3") then
+    writefile("Sliding Hot - Skrapp 2197.mp3", game:HttpGet("https://github.com/avilogist/1/blob/main/comet-songs/Sliding%20Hot%20-%20Skrapp%202197.mp3"))
+end
+
+local phonk2 = Instance.new("Sound", game:GetService("SoundService"))
+phonk2.SoundId = getcustomasset("blue.mp3")
+phonk2.Volume = getgenv().volume or 1
+phonk2.Looped = getgenv().loop == true
+
+phonk2:Play()
+if not phonk2.Looped then
+    phonk2.Ended:Connect(function()
+        phonk2:Destroy()
+    end)
+end
