8949271270c40a4ea170c8e903e8e273e006f5a9
avilogist <rbx.neo@gmail.com>
2025-12-25 15:32:24 +0000

    Update Gwallahs - #FREEPARANOIA(COMM DISS) PT 6.lua

--- comet-songs/Gwallahs - #FREEPARANOIA(COMM DISS) PT 6.lua
+++ comet-songs/Gwallahs - #FREEPARANOIA(COMM DISS) PT 6.lua
@@ -1,11 +1,11 @@
 -- Thx to https://github.com/XVCHub for the method and songs >.<
 
-if not isfile("Gwallahs - #FREEPARANOIA(COMM DISS) PT 6.mp3") then
-    writefile("Gwallahs - #FREEPARANOIA(COMM DISS) PT 6.mp3", game:HttpGet("https://github.com/avilogist/1/raw/refs/heads/main/comet-songs/Gwallahs%20-%20%23FREEPARANOIA(COMM%20DISS)%20PT%206.mp3"))
+if not isfile("Gwallahs.mp3") then
+    writefile("Gwallahs.mp3", game:HttpGet("https://github.com/avilogist/1/raw/refs/heads/main/comet-songs/Gwallahs%20-%20%23FREEPARANOIA(COMM%20DISS)%20PT%206.mp3"))
 end
 
 local phonk2 = Instance.new("Sound", game:GetService("SoundService"))
-phonk2.SoundId = getcustomasset("Gwallahs - #FREEPARANOIA(COMM DISS) PT 6.mp3")
+phonk2.SoundId = getcustomasset("Gwallahs.mp3")
 phonk2.Volume = getgenv().volume or 1
 phonk2.Looped = getgenv().loop == true
 
